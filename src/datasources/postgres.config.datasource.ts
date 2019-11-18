ec17139606f2fbf3a04343140e1b21ddb1a7188e
Stuart Caunt <caunt@ill.fr>
2019-11-18 14:54:47 +0100

    Prefix environment variable names

--- src/datasources/postgres.config.datasource.ts
+++ src/datasources/postgres.config.datasource.ts
@@ -4,10 +4,5 @@ dotenv.config();
 export const  dataConf={
   "name": "postgres",
   "connector": "postgresql",
-  "url": process.env.POSTGRES_URL,
-  "host": process.env.POSTGRES_HOST,
-  "port": process.env.POSTGRES_PORT,
-  "user": process.env.POSTGRESS_USER,
-  "password": process.env.POSTGRES_PASSWORD,
-  "database": process.env.POSTGRES_DATABASE,
+  "url": process.env.CLOUD_PROVIDER_K8S_POSTGRES_URL
 };
