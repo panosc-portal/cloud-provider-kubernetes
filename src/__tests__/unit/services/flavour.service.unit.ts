cabe92ea881175c8e14898b1c96e28240734adef
Stuart Caunt <caunt@ill.fr>
2019-11-25 10:16:46 +0100

    Add instance and flavour service tests

---
+++ src/__tests__/unit/services/flavour.service.unit.ts
@@ -0,0 +1,71 @@
+import {expect} from '@loopback/testlab';
+import { givenInitialisedDatabase } from '../../helpers/database.helper';
+import { getTestApplicationContext } from '../../helpers/context.helper';
+import { FlavourService } from '../../../services';
+import { Flavour } from '../../../models';
+
+describe('FlavourService', () => {
+  let flavourService: FlavourService;
+
+  before('getFlavourService', async () => {
+    flavourService = getTestApplicationContext().flavourService;
+  });
+
+  beforeEach('Initialise Database', givenInitialisedDatabase);
+
+  it('gets all flavours', async () => {
+    const flavours = await flavourService.getAll();
+    expect(flavours.length).to.equal(3);
+  });
+
+  it('gets a flavour', async () => {
+    const flavour = await flavourService.getById(1);
+
+    expect(flavour).to.not.be.null;
+    expect(flavour.name).to.equal("flavour 1");
+  });
+
+  it('saves a flavour', async () => {
+    const flavours = await flavourService.getAll();
+
+    const flavour = new Flavour({
+      name: 'flavour 3',
+      description: 'A new flavour',
+      memory: 1024,
+      cpu: 4
+    });
+    await flavourService.save(flavour);
+    expect(flavour.id).to.not.be.null;
+
+    const persistedFlavour = await flavourService.getById(flavour.id);
+    expect(persistedFlavour).to.not.be.null;
+  });
+
+  it('deletes a flavour', async () => {
+    let flavours = await flavourService.getAll();
+
+    const flavour = flavours.find(flavour => flavour.id == 3);
+
+    await flavourService.delete(flavour);
+
+    flavours = await flavourService.getAll();
+    expect(flavours.length).to.equal(2);
+  });
+
+
+  it('updates a flavour', async () => {
+    const flavours = await flavourService.getAll();
+
+    const flavour = flavours[0];
+    flavour.name = "A new name";
+
+    const persistedFlavour = await flavourService.save(flavour);
+    expect(persistedFlavour).to.not.be.null;
+    expect(persistedFlavour.id).to.equal(flavour.id);
+    expect(persistedFlavour.name).to.equal(flavour.name);
+
+    const flavoursAfterUpdate = await flavourService.getAll();
+    expect(flavoursAfterUpdate.length).to.equal(flavours.length);
+  });
+
+});
---
+++ src/__tests__/unit/services/instance.service.unit.ts
@@ -0,0 +1,107 @@
+import {expect} from '@loopback/testlab';
+import { givenInitialisedDatabase } from '../../helpers/database.helper';
+import { getTestApplicationContext } from '../../helpers/context.helper';
+import { InstanceService, ImageService, FlavourService } from '../../../services';
+import { Instance, Protocol } from '../../../models';
+
+describe('InstanceService', () => {
+  let instanceService: InstanceService;
+  let imageService: ImageService;
+  let flavourService: FlavourService;
+
+  before('getInstanceService', async () => {
+    imageService = getTestApplicationContext().imageService;
+    instanceService = getTestApplicationContext().instanceService;
+    flavourService = getTestApplicationContext().flavourService;
+  });
+
+  beforeEach('Initialise Database', givenInitialisedDatabase);
+
+  it('gets all instances', async () => {
+    const instances = await instanceService.getAll();
+
+    expect(instances.length).to.equal(2);
+  });
+
+  it('gets an instance', async () => {
+    const instance = await instanceService.getById(1);
+
+    expect(instance).to.not.be.null;
+    expect(instance.name).to.equal("instance 1");
+  });
+
+  it('saves an instance', async () => {
+    const instances = await instanceService.getAll();
+
+    const image = await imageService.getById(1);
+    const flavour = await flavourService.getById(2);
+
+    const instance = new Instance({
+      name: 'instance 3',
+      description: 'A new instance',
+      image: image,
+      flavour: flavour,
+      hostname: 'test.host.eu',
+      state: 'BUILDING',
+      currentCPU: 0,
+      currentMemory: 0,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      protocols: [new Protocol({name: 'ssh', port: 2222}), new Protocol({name: 'rdp', port: 1234})]
+    });
+
+    await instanceService.save(instance);
+    expect(instance.id).to.not.be.null;
+
+    const persistedInstance = await instanceService.getById(instance.id);
+    expect(persistedInstance).to.not.be.null;
+    expect(persistedInstance.image).to.not.be.null;
+    expect(persistedInstance.image.id).to.equal(1);
+    expect(persistedInstance.flavour).to.not.be.null;
+    expect(persistedInstance.flavour.id).to.equal(2);
+    expect(persistedInstance.protocols.length).to.equal(2);
+    persistedInstance.protocols.forEach(protocol => {
+      expect(protocol.id).to.not.be.null;
+    })
+  });
+
+  it('deletes an instance', async () => {
+    let instances = await instanceService.getAll();
+
+    const instance = instances[0];
+    const image = instance.image;
+    expect(image).to.not.be.null;
+    const flavour = instance.flavour;
+    expect(flavour).to.not.be.null;
+
+    await instanceService.delete(instance);
+
+    instances = await instanceService.getAll();
+    expect(instances.length).to.equal(1);
+
+    // Make sure image not deleted
+    const persistedImage = imageService.getById(image.id);
+    expect(persistedImage).to.not.be.null;
+
+    // Make sure flavour not deleted
+    const persistedFlavour = flavourService.getById(flavour.id);
+    expect(persistedFlavour).to.not.be.null;
+  });
+
+
+  it('updates an instance', async () => {
+    const instances = await instanceService.getAll();
+
+    const instance = instances[0];
+    instance.name = "A new name";
+
+    const persistedInstance = await instanceService.save(instance);
+    expect(persistedInstance).to.not.be.null;
+    expect(persistedInstance.id).to.equal(instance.id);
+    expect(persistedInstance.name).to.equal(instance.name);
+
+    const instancesAfterUpdate = await instanceService.getAll();
+    expect(instancesAfterUpdate.length).to.equal(instances.length);
+  });
+
+});
